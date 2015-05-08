b993a3f8d3aeb85b27dd5331e11514811393f035
Tad Thorley <phaedryx@gmail.com>
2015-05-07 21:55:07 -0600

    simple event streaming

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
 require 'tilt/erubis'
 
+set server: 'puma', connections: []
+
 get '/' do
   erb :index
+end
+
+get '/stream', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    settings.connections << out
+    out.callback { settings.connections.delete(out) }
+  end
+end
+
+post '/' do
+  settings.connections.each { |out| out << "data: #{params[:msg]}\n\n" }
+  204
 end
